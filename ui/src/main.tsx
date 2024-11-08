987b7b6c49c9c0b543bbcef1853c9ddec966767d
Sledmine <40512223+Sledmine@users.noreply.github.com>
2024-11-07 23:29:59 -0600

    add neutralino lib init

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { init } from "@neutralinojs/lib";
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+init();
